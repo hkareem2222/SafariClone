af2363d99ae067bad0c02b143c85263f25042bb9
jdivittorio3 <jdivittorio3@gmail.com>
2015-05-13 16:13:33 -0500

    code updated, without the scroll view and views updated

--- SafariClone/ViewController.m
+++ SafariClone/ViewController.m
@@ -9,9 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIAlertViewDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @end
 
@@ -19,8 +22,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self goToURLString:@"http://www.google.com"];
 
-    [self goToURLString:@"http://www.mobilemakers.co"];
+    
+
 }
 
 -(void)goToURLString:(NSString *)urlString {
@@ -30,9 +35,16 @@
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [self goToURLString:textField.text];
-    [textField resignFirstResponder];
-    return YES;
+    if ([textField.text containsString:@"http://"]) {
+        [self goToURLString:textField.text];
+        [textField resignFirstResponder];
+        return YES;
+    } else {
+        NSString *updatedUrl = [NSString stringWithFormat:@"http://www.%@", textField.text];
+        [self goToURLString:updatedUrl];
+        [textField resignFirstResponder];
+        return YES;
+    }
 }
 
 -(void)webViewDidStartLoad:(UIWebView *)webView {
@@ -40,7 +52,11 @@
 }
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self.backButton setEnabled:[self.webView canGoBack]];
+    [self.forwardButton setEnabled:[self.webView canGoForward]];
     [self.spinner stopAnimating];
+    NSString *currentURL = self.webView.request.URL.absoluteString;
+    self.textField.text = currentURL;
 }
 
 -(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
@@ -56,22 +72,29 @@
 
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     if (buttonIndex == 1) {
-        [self goToURLString:@"http://www.mobilemakers.co"];
+        [self goToURLString:@"http://www.google.com"];
     }
 }
 
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
-
+    [self.webView goBack];
 }
 
 - (IBAction)onForwardButtonPressed:(UIButton *)sender {
-
+    [self.webView goForward];
 }
 - (IBAction)onStopLoadingButtonPressed:(UIButton *)sender {
-
+    [self.webView stopLoading];
 }
 - (IBAction)onReloadButtonPressed:(UIButton *)sender {
-
+    [self.webView reload];
+}
+- (IBAction)comingSoonButtonPressed:(UIButton *)sender {
+    UIAlertView *alert = [UIAlertView new];
+    alert.title = @"Coming Soon!";
+    [alert addButtonWithTitle:@"Dismiss"];
+    alert.delegate = self;
+    [alert show];
 }
 
 //- (IBAction)onBackButtonPressed:(UIBarButtonItem *)sender {
